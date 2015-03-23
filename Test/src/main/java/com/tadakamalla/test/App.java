05a9dc4d00ec1d0187b5c214661cebdcb735cbae
Ashok Tadakamalla <atadakamalla@expedia.com>
2015-03-22 23:25:58 -0700

    Added Unit test cases to Fibonacci()

--- Test/src/main/java/com/tadakamalla/test/App.java
+++ Test/src/main/java/com/tadakamalla/test/App.java
@@ -6,30 +6,60 @@ public class App {
 		
 		System.out.println ("Hello world");
 		//Iterative style for fibonacci
-		Fibonacci(10);
 		
+		int count = 10;
+		
+	   int[] numbers = Fibonacci(count);
+	   System.out.println ("Fibonacci Iterative");
+	   for (int i = 0; i<count; i++)
+	   {
+		   System.out.println (numbers[i]);
+	   }
+		
+	   System.out.println ("Fibonacci Recursive");
+	   int recursiveNumbers[] = new int[count+1];
 		//Recursive style for fibonacci.
-		for (int i = 1; i<=5; i++){
-			FibonacciRecursive(i);
-					}
+		for (int i = 1; i<=count; i++){
+			recursiveNumbers[i] = FibonacciRecursive(i);
+			}
+		for (int i = 0; i<count; i++)
+		   {
+			   System.out.println (recursiveNumbers[i]);
+		   }
+		
 	}
 	
-	public static void Fibonacci (int number) {
+	public static int[] Fibonacci (int number) {
+		if (number <= 0) {
+			return null;
+		}
 		int num1 = 0;
 		int num2 = 1;
 		int sum = 0;
-		System.out.println ("Fibonacci Series");
+		
+		int[] fibNumbers = new int[number];
+		
+		fibNumbers[0] = num1;
+		fibNumbers[1] = num2;
+		
 		//Iterative
-		for(int index = 0; index<=number; index++){
+		for(int index = 2; index<=number-1; index++){
 			sum = num1 + num2;
 			num1 = num2;
 			num2 = sum;
 			
-			System.out.println(sum);
+			fibNumbers[index] = sum;
 		}
+		
+		return fibNumbers;
 	}
 	
+
+	
 	public static int FibonacciRecursive(int number){
+		if(number <= 0){
+			return 0;
+		}
 		if (number==1 || number ==2){
 			return 1;
 		}
--- Test/src/test/java/com/tadakamalla/test/AppTest.java
+++ Test/src/test/java/com/tadakamalla/test/AppTest.java
@@ -1,5 +1,6 @@
 package com.tadakamalla.test;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 
@@ -8,6 +9,41 @@ public class AppTest {
 	@Test
 	public void testmain(){
 	    App.main(null);
+	}
+	
+	@Test
+	public void FibonacciTest(){
+		int number = 5;
+		int[] result = App.Fibonacci(number);
+		
+		int[] expected = {0, 1, 1, 2, 3};
+		
+		assertNotNull(result);
+		assertEquals(expected.length,  result.length);
+		// assertEquals(expected, result);
+		assertEquals(expected[number-1],result[number-1]);
+	}
+	
+	@Test
+	public void FibonacciTestWithZeroInput(){
+		int[] result = App.Fibonacci(0);
+			
+		assertNull(result);
+	}
+	
+	@Test
+	public void FibonacciTestwithNegitiveInput(){
+		int[] result = App.Fibonacci(-1);
+			
+		assertNull(result);
+	}
+	
+	public void FibonacciTestwithOneInput(){
+		int[] result = App.Fibonacci(1);
+		int[] expected = {0};
+		assertNotNull(result);
+		assertEquals(expected.length,  result.length);
+		assertEquals(expected, result);
 	}
 
 }
